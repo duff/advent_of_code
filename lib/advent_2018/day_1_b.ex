50fd8d6dd2013ac9d4d600890ebd68839bf20f96
Duff OMelia <duff@omelia.org>
2018-12-03 07:53:04 -0500

    Use Stream.cycle
    Pretty neat. Learned from José's twitch.

--- lib/advent_2018/day_1_b.ex
+++ lib/advent_2018/day_1_b.ex
@@ -2,28 +2,15 @@ defmodule Advent2018.Day1B do
   def first_duplicate(input) do
     input
     |> as_integers()
-    |> do_first_duplicate()
-  end
-
-  defp do_first_duplicate(original_deltas) do
-    do_first_duplicate(original_deltas, 0, MapSet.new(), original_deltas)
-  end
-
-  defp do_first_duplicate([next | rest], current_frequency, past_frequencies, original_deltas) do
-    if MapSet.member?(past_frequencies, current_frequency) do
-      current_frequency
-    else
-      do_first_duplicate(
-        rest,
-        current_frequency + next,
-        MapSet.put(past_frequencies, current_frequency),
-        original_deltas
-      )
-    end
-  end
-
-  defp do_first_duplicate([], current_frequency, past_frequencies, original_deltas) do
-    do_first_duplicate(original_deltas, current_frequency, past_frequencies, original_deltas)
+    |> Stream.cycle()
+    |> Enum.reduce_while({0, MapSet.new([0])}, fn each, {current_frequency, past_frequencies} ->
+      new_frequency = current_frequency + each
+      if new_frequency in past_frequencies do
+        {:halt, new_frequency}
+      else
+        {:cont, {new_frequency, MapSet.put(past_frequencies, new_frequency)}}
+      end
+    end)
   end
 
   defp as_integers(input) do
