250f7d2f053dba61f3f8724d605d75aead3f609e
Duff OMelia <duff@omelia.org>
2018-12-10 06:26:24 -0500

    Fix 8A
    Now works with the real input in addition to the test input.

--- lib/advent_2018/day_8_a.ex
+++ lib/advent_2018/day_8_a.ex
@@ -1,20 +1,59 @@
+defmodule Advent2018.Tree do
+  defstruct children: [], meta: []
+
+  alias Advent2018.Tree
+
+  def new do
+    %Tree{}
+  end
+end
+
 defmodule Advent2018.Day8A do
+  alias Advent2018.Tree
+
   def metadata_sum(input) do
     input
-    |> String.split()
-    |> Enum.map(&String.to_integer/1)
-    |> metadata_sum([])
+    |> tree
+    |> retrieve_meta([])
+    |> Enum.sum()
   end
 
-  defp metadata_sum([0, meta_count | tail], acc) do
-    metadata_sum(Enum.drop(tail, meta_count), Enum.take(tail, meta_count) ++ acc)
+  def tree(input) do
+    {tree, []} =
+      input
+      |> String.split()
+      |> Enum.map(&String.to_integer/1)
+      |> build_tree()
+
+    tree
   end
 
-  defp metadata_sum([_child_count, meta_count | tail], acc) do
-    metadata_sum(Enum.drop(tail, -meta_count), Enum.take(tail, -meta_count) ++ acc)
+  defp build_tree([child_count, meta_count | rest]) do
+    {tree, rest} = add_childen_to_tree(child_count, rest, Tree.new())
+    {meta, rest} = Enum.split(rest, meta_count)
+
+    {%{tree | meta: meta}, rest}
   end
 
-  defp metadata_sum([], acc) do
-    Enum.sum(acc)
+  defp add_childen_to_tree(0, rest, tree) do
+    tree = Map.update!(tree, :children, fn value -> Enum.reverse(value) end)
+    {tree, rest}
+  end
+
+  defp add_childen_to_tree(num, rest, tree) do
+    {child, rest} = build_tree(rest)
+
+    tree = Map.update!(tree, :children, fn value -> [child | value] end)
+    add_childen_to_tree(num - 1, rest, tree)
+  end
+
+  defp retrieve_meta(%Tree{meta: meta, children: []}, acc) do
+    acc ++ meta
+  end
+
+  defp retrieve_meta(%Tree{meta: meta, children: children}, acc) do
+    Enum.reduce(children, acc, fn child, acc ->
+      retrieve_meta(child, acc)
+    end) ++ meta
   end
 end
--- test/lib/advent_2018/day_8_a_test.exs
+++ test/lib/advent_2018/day_8_a_test.exs
@@ -2,6 +2,56 @@ defmodule Advent2018.Day8ATest do
   use ExUnit.Case, async: true
 
   alias Advent2018.Day8A
+  alias Advent2018.Tree
+
+  test "simple tree with no children" do
+    input = "0 3 2 3 4"
+
+    assert Day8A.tree(input) ==
+             %Tree{
+               children: [],
+               meta: [2, 3, 4]
+             }
+  end
+
+  test "tree with one child" do
+    input = "1 2 0 3 2 3 4 5 6"
+
+    assert Day8A.tree(input) ==
+             %Tree{
+               children: [
+                 %Tree{
+                   children: [],
+                   meta: [2, 3, 4]
+                 }
+               ],
+               meta: [5, 6]
+             }
+  end
+
+  test "tree" do
+    input = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"
+
+    assert Day8A.tree(input) ==
+             %Tree{
+               children: [
+                 %Tree{
+                   children: [],
+                   meta: [10, 11, 12]
+                 },
+                 %Tree{
+                   children: [
+                     %Tree{
+                       children: [],
+                       meta: [99]
+                     }
+                   ],
+                   meta: [2]
+                 }
+               ],
+               meta: [1, 1, 2]
+             }
+  end
 
   test "metadata_sum" do
     input = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"
@@ -11,6 +61,6 @@ defmodule Advent2018.Day8ATest do
   @tag :real
   test "metadata_sum real" do
     input = File.read!("test/lib/advent_2018/input/day8.txt") |> String.trim()
-    assert Day8A.metadata_sum(input) == 38449
+    assert Day8A.metadata_sum(input) == 44338
   end
 end
