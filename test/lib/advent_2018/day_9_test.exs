b5c19b790eda114c67914d156eef61d60e99399e
Duff OMelia <duff@omelia.org>
2018-12-11 18:05:55 -0500

    Bag on Day
    Weak! Can't spend more time on this one.

--- test/lib/advent_2018/day_9_test.exs
+++ test/lib/advent_2018/day_9_test.exs
@@ -9,11 +9,14 @@ defmodule Advent2018.Day9Test do
     assert Day9.score(17, 1104) == 2764
     assert Day9.score(21, 6111) == 54718
     assert Day9.score(30, 5807) == 37305
+
+    # Not sure why this is failing
     # assert Day9.score(13, 7999) == 146_373
   end
 
   @tag :real
   test "score real" do
+    # Not sure why this is failing on the site. :(
     assert Day9.score(455, 71223) == 393_972
   end
 end
