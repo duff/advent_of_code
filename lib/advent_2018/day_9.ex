26c930a401e0b338988525dd6ff106e816086e52
Duff OMelia <duff@omelia.org>
2018-12-11 08:20:40 -0500

    Start on Day 9
    I think it's close. All tests passing except 1. :(

--- lib/advent_2018/day_9.ex
+++ lib/advent_2018/day_9.ex
@@ -1,5 +1,85 @@
+defmodule Advent2018.Day9.Game do
+  defstruct marbles: [0],
+            current_marble_index: 0,
+            current_player: 1,
+            player_count: nil,
+            seeking_marble_worth: nil,
+            last_marble_index: nil,
+            next_marble: 1,
+            scores: %{}
+end
+
 defmodule Advent2018.Day9 do
-  def score(player_count, last_marble) do
-    0
+  alias Advent2018.Day9.Game
+
+  def score(player_count, last_marble_played) do
+    initialize_game(player_count, last_marble_played)
+    |> play()
+    |> find_high_score()
+  end
+
+  defp play(%Game{next_marble: same, last_marble_index: same} = game) do
+    game
+  end
+
+  defp play(game) do
+    game
+    |> insert_next_marble()
+    |> increment_next_marble()
+    |> increment_current_player()
+    |> play()
+  end
+
+  defp insert_next_marble(%Game{next_marble: next_marble} = game) when rem(next_marble, 23) == 0 do
+    bonus_index = bonus_marble_index(game)
+    {bonus_marble, marbles} = List.pop_at(game.marbles, bonus_index)
+
+    added_to_score = game.next_marble + bonus_marble
+    new_score = Map.update(game.scores, game.current_player, added_to_score, &(&1 + added_to_score))
+
+    %{game | scores: new_score, marbles: marbles, current_marble_index: bonus_index}
+  end
+
+  defp insert_next_marble(game) do
+    new_index = new_index(game)
+    marbles = List.insert_at(game.marbles, new_index, game.next_marble)
+    %{game | marbles: marbles, current_marble_index: new_index}
+  end
+
+  defp bonus_marble_index(%Game{current_marble_index: current}) when current >= 7 do
+    current - 7
+  end
+
+  defp bonus_marble_index(%Game{current_marble_index: current}) do
+    -(7 - current)
+  end
+
+  defp new_index(%Game{current_marble_index: current, marbles: marbles}) when current == length(marbles) - 1 do
+    1
+  end
+
+  defp new_index(%Game{current_marble_index: current}) do
+    current + 2
+  end
+
+  defp increment_next_marble(%Game{next_marble: next_marble} = game) do
+    %{game | next_marble: next_marble + 1}
+  end
+
+  defp increment_current_player(%Game{current_player: same, player_count: same} = game) do
+    %{game | current_player: 1}
+  end
+
+  defp increment_current_player(%Game{current_player: current_player} = game) do
+    %{game | current_player: current_player + 1}
+  end
+
+  defp initialize_game(player_count, last_marble_played) do
+    %Game{player_count: player_count, last_marble_index: last_marble_played + 1}
+  end
+
+  defp find_high_score(%Game{scores: scores}) do
+    {_player, score} = Enum.max_by(scores, fn {_key, value} -> value end)
+    score
   end
 end
--- test/lib/advent_2018/day_9_test.exs
+++ test/lib/advent_2018/day_9_test.exs
@@ -6,14 +6,14 @@ defmodule Advent2018.Day9Test do
   test "score" do
     assert Day9.score(9, 25) == 32
     assert Day9.score(10, 1618) == 8317
-    assert Day9.score(13, 7999) == 146_373
     assert Day9.score(17, 1104) == 2764
     assert Day9.score(21, 6111) == 54718
     assert Day9.score(30, 5807) == 37305
+    assert Day9.score(13, 7999) == 146_373
   end
 
   @tag :real
   test "score real" do
-    assert Day9.score(455, 5807) == 0
+    assert Day9.score(455, 5807) == 8268
   end
 end
