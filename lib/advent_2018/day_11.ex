cc0bdb64cbcebf3c4ac45a15d82e3b8efe0afcd5
Duff OMelia <duff@omelia.org>
2018-12-15 15:13:39 -0500

    Implement 11A

--- lib/advent_2018/day_11.ex
+++ lib/advent_2018/day_11.ex
@@ -1,2 +1,39 @@
 defmodule Advent2018.Day11 do
+  @grid_size 300
+
+  def power_level({x, y}, serial_number) do
+    rack_id = x + 10
+    power_level = (rack_id * y + serial_number) * rack_id
+    hundreds_digit(power_level) - 5
+  end
+
+  def largest_power_grid_of_nine(serial_number) do
+    [{found, _}, _] =
+      for y <- 1..(@grid_size - 3),
+          x <- 1..(@grid_size - 3) do
+        grid_of_nine = {{x, y}, {x + 2, y + 2}}
+        [grid_of_nine, power_level_of_grid_of_nine(grid_of_nine, serial_number)]
+      end
+      |> Enum.max_by(fn [_, power_level] -> power_level end)
+
+    found
+  end
+
+  defp power_level_of_grid_of_nine({{top_left_x, top_left_y}, {bottom_right_x, bottom_right_y}}, serial_number) do
+    for x <- top_left_x..bottom_right_x,
+        y <- top_left_y..bottom_right_y do
+      {x, y}
+    end
+    |> Enum.map(&power_level(&1, serial_number))
+    |> Enum.sum()
+  end
+
+  defp hundreds_digit(number) do
+    number
+    |> Integer.to_string()
+    |> String.codepoints()
+    |> Enum.reverse()
+    |> Enum.at(2, "0")
+    |> String.to_integer()
+  end
 end
--- test/lib/advent_2018/day_11_test.exs
+++ test/lib/advent_2018/day_11_test.exs
@@ -1,6 +1,22 @@
 defmodule Advent2018.Day11Test do
   use ExUnit.Case, async: true
 
+  alias Advent2018.Day11
+
   test "power_level" do
+    assert Day11.power_level({3, 5}, 8) == 4
+    assert Day11.power_level({122, 79}, 57) == -5
+    assert Day11.power_level({217, 196}, 39) == 0
+    assert Day11.power_level({101, 153}, 71) == 4
+  end
+
+  test "largest_power_grid_of_nine" do
+    assert Day11.largest_power_grid_of_nine(18) == {33, 45}
+    assert Day11.largest_power_grid_of_nine(42) == {21, 61}
+  end
+
+  @tag :real
+  test "largest_power_grid_of_nine real" do
+    assert Day11.largest_power_grid_of_nine(1309) == {20, 43}
   end
 end
