54b9e35c05baa99a74550c610f78ebf454586706
Duff OMelia <duff@omelia.org>
2018-12-15 08:32:17 -0500

    Implement Day 10A

---
+++ lib/advent_2018/day_10.ex
@@ -0,0 +1,95 @@
+defmodule Advent2018.Day10.Position do
+  defstruct [:x, :y, :velocity_x, :velocity_y]
+end
+
+defmodule Advent2018.Day10.Rectangle do
+  defstruct [:top_left, :bottom_right]
+  alias Advent2018.Day10.Rectangle
+
+  def area(%Rectangle{top_left: {x1, y1}, bottom_right: {x2, y2}}) do
+    abs((x1 - x2) * (y1 - y2))
+  end
+end
+
+defmodule Advent2018.Day10 do
+  alias Advent2018.Day10.{Position, Rectangle}
+
+  def print_eventual_message(input) do
+    positions = to_positions(input)
+    {positions, _} = smallest_sky(positions)
+    draw(positions)
+  end
+
+  def to_positions(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse_line/1)
+  end
+
+  def move(%Position{x: x, y: y, velocity_x: velocity_x, velocity_y: velocity_y} = position) do
+    %{position | x: x + velocity_x, y: y + velocity_y}
+  end
+
+  def bounding_rectangle(positions) do
+    {min_x, max_x} = Enum.min_max_by(positions, fn %Position{x: x} -> x end)
+    {min_y, max_y} = Enum.min_max_by(positions, fn %Position{y: y} -> y end)
+    %Rectangle{top_left: {min_x.x - 1, min_y.y - 1}, bottom_right: {max_x.x + 1, max_y.y + 1}}
+  end
+
+  def area(positions) do
+    positions
+    |> bounding_rectangle()
+    |> Rectangle.area()
+  end
+
+  def smallest_sky(positions) do
+    Enum.reduce(1..50_000, [{positions, area(positions)}], fn _each, [{previous_positions, _} | _] = acc ->
+      next_positions = Enum.map(previous_positions, &move/1)
+      next_area = area(next_positions)
+      [{next_positions, next_area} | acc]
+    end)
+    |> Enum.min_by(fn {_, area} -> area end)
+  end
+
+  def draw(positions) do
+    %Rectangle{top_left: {x1, y1}, bottom_right: {x2, y2}} = bounding_rectangle(positions)
+
+    rows =
+      for y <- y1..y2 do
+        row(x1, x2, y, positions)
+      end
+      |> Enum.join("\n")
+
+    IO.puts(rows)
+  end
+
+  defp row(x1, x2, y, positions) do
+    for x <- x1..x2, into: "" do
+      if includes(positions, x, y) do
+        "*"
+      else
+        " "
+      end
+    end
+  end
+
+  defp includes(positions, x, y) do
+    Enum.any?(positions, fn each ->
+      each.x == x && each.y == y
+    end)
+  end
+
+  defp parse_line(line) do
+    [x, y, velocity_x, velocity_y] = retrieve_ints(line)
+    %Position{x: x, y: y, velocity_x: velocity_x, velocity_y: velocity_y}
+  end
+
+  defp retrieve_ints(line) do
+    Regex.run(position_regex(), line, capture: :all_but_first)
+    |> Enum.map(fn each -> String.trim(each) |> String.to_integer() end)
+  end
+
+  defp position_regex do
+    ~r/position=<([- ]*\d*),([- ]*\d*)> velocity=<([- ]*\d*),([- ]*\d*)>/
+  end
+end
---
+++ test/lib/advent_2018/day_10_test.exs
@@ -0,0 +1,78 @@
+defmodule Advent2018.Day10Test do
+  use ExUnit.Case, async: true
+
+  alias Advent2018.Day10
+  alias Advent2018.Day10.{Position, Rectangle}
+
+  test "to_positions" do
+    input = """
+    position=<-3,  6> velocity=< 2, -1>
+    position=<14,  7> velocity=<-2,  0>
+    position=< -9988, -51087> velocity=< 1,  5>
+    """
+
+    expected = [
+      %Position{x: -3, y: 6, velocity_x: 2, velocity_y: -1},
+      %Position{x: 14, y: 7, velocity_x: -2, velocity_y: 0},
+      %Position{x: -9988, y: -51087, velocity_x: 1, velocity_y: 5}
+    ]
+
+    assert Day10.to_positions(input) == expected
+  end
+
+  test "move" do
+    position = %Position{x: 3, y: 9, velocity_x: 1, velocity_y: -2}
+    assert %Position{x: 4, y: 7} = Day10.move(position)
+  end
+
+  test "bounding_rectangle" do
+    positions = Day10.to_positions(example_input())
+    assert %Rectangle{top_left: {-7, -5}, bottom_right: {16, 12}} == Day10.bounding_rectangle(positions)
+  end
+
+  test "area of rectangle" do
+    rect = %Rectangle{top_left: {-2, -3}, bottom_right: {4, 5}}
+    assert Rectangle.area(rect) == 48
+  end
+
+  test "smallest_sky" do
+    positions = Day10.to_positions(example_input())
+    assert {positions, area} = Day10.smallest_sky(positions)
+  end
+
+  defp example_input do
+    """
+    position=< 9,  1> velocity=< 0,  2>
+    position=< 7,  0> velocity=<-1,  0>
+    position=< 3, -2> velocity=<-1,  1>
+    position=< 6, 10> velocity=<-2, -1>
+    position=< 2, -4> velocity=< 2,  2>
+    position=<-6, 10> velocity=< 2, -2>
+    position=< 1,  8> velocity=< 1, -1>
+    position=< 1,  7> velocity=< 1,  0>
+    position=<-3, 11> velocity=< 1, -2>
+    position=< 7,  6> velocity=<-1, -1>
+    position=<-2,  3> velocity=< 1,  0>
+    position=<-4,  3> velocity=< 2,  0>
+    position=<10, -3> velocity=<-1,  1>
+    position=< 5, 11> velocity=< 1, -2>
+    position=< 4,  7> velocity=< 0, -1>
+    position=< 8, -2> velocity=< 0,  1>
+    position=<15,  0> velocity=<-2,  0>
+    position=< 1,  6> velocity=< 1,  0>
+    position=< 8,  9> velocity=< 0, -1>
+    position=< 3,  3> velocity=<-1,  1>
+    position=< 0,  5> velocity=< 0, -1>
+    position=<-2,  2> velocity=< 2,  0>
+    position=< 5, -2> velocity=< 1,  2>
+    position=< 1,  4> velocity=< 2,  1>
+    position=<-2,  7> velocity=< 2, -2>
+    position=< 3,  6> velocity=<-1, -1>
+    position=< 5,  0> velocity=< 1,  0>
+    position=<-6,  0> velocity=< 2,  0>
+    position=< 5,  9> velocity=< 1, -2>
+    position=<14,  7> velocity=<-2,  0>
+    position=<-3,  6> velocity=< 2, -1>
+    """
+  end
+end
