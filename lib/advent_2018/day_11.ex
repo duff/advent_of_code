9333820a40bdec27760703fdca38ebd6f4dc69d7
Duff OMelia <duff@omelia.org>
2018-12-17 17:02:32 -0500

    Speed up 11B
    Whewwww...
    
    Had to read https://en.wikipedia.org/wiki/Summed-area_table really
    carefully. And I think the formula doesn't match the table on the right
    on that page. The table is right.

--- lib/advent_2018/day_11.ex
+++ lib/advent_2018/day_11.ex
@@ -1,53 +1,74 @@
 defmodule Advent2018.Day11 do
-  @grid_size 300
+  def part_a(serial_number) do
+    {{found, _}, _} = largest_power_grid(300, 3, 3, serial_number)
+    found
+  end
+
+  def part_b(serial_number) do
+    {{{x, y}, size}, _} = largest_power_grid(300, 1, 300, serial_number)
+    {x, y, size}
+  end
 
   def power_level({x, y}, serial_number) do
     rack_id = x + 10
     power_level = (rack_id * y + serial_number) * rack_id
-    hundreds_digit(power_level) - 5
+
+    power_level
+    |> div(100)
+    |> rem(10)
+    |> Kernel.-(5)
   end
 
-  def largest_power_grid_of_nine(serial_number) do
-    [{found, _}, _] =
-      for y <- 1..(@grid_size - 3),
-          x <- 1..(@grid_size - 3) do
-        grid = {{x, y}, {x + 2, y + 2}}
-        [grid, power_level_of_grid(grid, serial_number)]
-      end
-      |> Enum.max_by(fn [_, power_level] -> power_level end)
-
-    found
-  end
-
-  def largest_power_grid(serial_number) do
-    found =
-      for y <- 1..@grid_size,
-          x <- 1..@grid_size,
-          size <- 1..@grid_size,
-          x + size <= @grid_size,
-          y + size <= @grid_size do
-        grid = {{x, y}, {x + size, y + size}}
-        %{x: x, y: y, size: 1 + size, total_power: power_level_of_grid(grid, serial_number)}
-      end
-      |> Enum.max_by(fn each -> Map.get(each, :total_power) end)
-
-    {found.x, found.y, found.size}
-  end
-
-  defp power_level_of_grid({{top_left_x, top_left_y}, {bottom_right_x, bottom_right_y}}, serial_number) do
-    for x <- top_left_x..bottom_right_x,
-        y <- top_left_y..bottom_right_y do
+  def summed_area_table(grid_size, input_table) do
+    for y <- 1..grid_size, x <- 1..grid_size do
       {x, y}
     end
-    |> Enum.reduce(0, fn each, acc -> acc + power_level(each, serial_number) end)
+    |> Enum.reduce(%{}, fn {x, y}, acc ->
+      value = summed_area_position(x, y, input_table, acc)
+      Map.put(acc, {x, y}, value)
+    end)
   end
 
-  defp hundreds_digit(number) do
-    number
-    |> Integer.to_string()
-    |> String.codepoints()
-    |> Enum.reverse()
-    |> Enum.at(2, "0")
-    |> String.to_integer()
+  defp summed_area_position(x, y, input_table, summed_area_table) do
+    a = Map.get(input_table, {x, y})
+    b = Map.get(summed_area_table, {x, y - 1}, 0)
+    c = Map.get(summed_area_table, {x - 1, y}, 0)
+    d = Map.get(summed_area_table, {x - 1, y - 1}, 0)
+
+    a + b + c - d
+  end
+
+  defp largest_power_grid(max_grid_size, min_area_size, max_area_size, serial_number) do
+    table = generate_summed_area_table(max_grid_size, serial_number)
+
+    for y <- 1..max_grid_size,
+        x <- 1..max_grid_size,
+        size <- min_area_size..max_area_size,
+        x + size <= max_grid_size,
+        y + size <= max_grid_size do
+      {{{x, y}, size}, power_level_of_grid(x, y, size, table)}
+    end
+    |> Enum.max_by(fn {_, power_level} -> power_level end)
+  end
+
+  defp power_level_of_grid(x, y, size, table) do
+    a = Map.get(table, {x - 1, y - 1}, 0)
+    b = Map.get(table, {x - 1 + size, y - 1}, 0)
+    c = Map.get(table, {x - 1, y + size - 1}, 0)
+    d = Map.get(table, {x + size - 1, y + size - 1}, 0)
+    d + a - b - c
+  end
+
+  defp power_levels(grid_size, serial_number) do
+    for y <- 1..grid_size,
+        x <- 1..grid_size,
+        into: %{} do
+      {{x, y}, power_level({x, y}, serial_number)}
+    end
+  end
+
+  defp generate_summed_area_table(max_grid_size, serial_number) do
+    power_levels = power_levels(max_grid_size, serial_number)
+    summed_area_table(max_grid_size, power_levels)
   end
 end
--- test/lib/advent_2018/day_11_test.exs
+++ test/lib/advent_2018/day_11_test.exs
@@ -10,23 +10,82 @@ defmodule Advent2018.Day11Test do
     assert Day11.power_level({101, 153}, 71) == 4
   end
 
-  test "largest_power_grid_of_nine" do
-    assert Day11.largest_power_grid_of_nine(18) == {33, 45}
-    assert Day11.largest_power_grid_of_nine(42) == {21, 61}
+  test "summed_area_table_2" do
+    input = %{
+      {1, 1} => 31,
+      {1, 2} => 12,
+      {1, 3} => 13,
+      {1, 4} => 24,
+      {1, 5} => 30,
+      {2, 1} => 2,
+      {2, 2} => 26,
+      {2, 3} => 17,
+      {2, 4} => 23,
+      {2, 5} => 8,
+      {3, 1} => 4,
+      {3, 2} => 9,
+      {3, 3} => 21,
+      {3, 4} => 15,
+      {3, 5} => 28,
+      {4, 1} => 33,
+      {4, 2} => 10,
+      {4, 3} => 22,
+      {4, 4} => 16,
+      {4, 5} => 27,
+      {5, 1} => 5,
+      {5, 2} => 29,
+      {5, 3} => 20,
+      {5, 4} => 14,
+      {5, 5} => 11
+    }
+
+    assert Day11.summed_area_table(5, input) ==
+             %{
+               {1, 1} => 31,
+               {1, 2} => 43,
+               {1, 3} => 56,
+               {1, 4} => 80,
+               {1, 5} => 110,
+               {2, 1} => 33,
+               {2, 2} => 71,
+               {2, 3} => 101,
+               {2, 4} => 148,
+               {2, 5} => 186,
+               {3, 1} => 37,
+               {3, 2} => 84,
+               {3, 3} => 135,
+               {3, 4} => 197,
+               {3, 5} => 263,
+               {4, 1} => 70,
+               {4, 2} => 127,
+               {4, 3} => 200,
+               {4, 4} => 278,
+               {4, 5} => 371,
+               {5, 1} => 75,
+               {5, 2} => 161,
+               {5, 3} => 254,
+               {5, 4} => 346,
+               {5, 5} => 450
+             }
   end
 
-  # test "largest_power_grid" do
-  #   assert Day11.largest_power_grid(18) == {90, 269, 16}
-  #   assert Day11.largest_power_grid(42) == {232, 251, 12}
-  # end
+  test "part_a" do
+    assert Day11.part_a(18) == {33, 45}
+    assert Day11.part_a(42) == {21, 61}
+  end
+
+  test "part_b" do
+    assert Day11.part_b(18) == {90, 269, 16}
+    assert Day11.part_b(42) == {232, 251, 12}
+  end
 
   @tag :real
-  test "largest_power_grid_of_nine real" do
-    assert Day11.largest_power_grid_of_nine(1309) == {20, 43}
+  test "part_a real" do
+    assert Day11.part_a(1309) == {20, 43}
   end
 
-  # @tag :real
-  # test "largest_power_grid real" do
-  #   assert Day11.largest_power_grid(1309) == {0, 0, 0}
-  # end
+  @tag :real
+  test "largest_power_grid real" do
+    assert Day11.part_b(1309) == {233, 271, 13}
+  end
 end
