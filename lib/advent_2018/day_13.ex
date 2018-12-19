995732551b57921e76f1c4b05da09a31b066bec9
Duff OMelia <duff@omelia.org>
2018-12-19 17:29:12 -0500

    Setup Day 13

---
+++ lib/advent_2018/day_13.ex
@@ -0,0 +1,4 @@
+defmodule Advent2018.Day13 do
+  def part_a(input) do
+  end
+end
---
+++ test/lib/advent_2018/day_13_test.exs
@@ -0,0 +1,32 @@
+defmodule Advent2018.Day13Test do
+  use ExUnit.Case, async: true
+
+  alias Advent2018.Day13
+
+  test "part_a simplest track" do
+    input = """
+    |
+    v
+    |
+    |
+    |
+    ^
+    |
+    """
+
+    assert Day13.part_a(input) == {0, 3}
+  end
+
+  test "part_a larger track" do
+    input = """
+    /->-\
+    |   |  /----\
+    | /-+--+-\  |
+    | | |  | v  |
+    \-+-/  \-+--/
+      \------/
+    """
+
+    assert Day13.part_a(input) == {7, 3}
+  end
+end
