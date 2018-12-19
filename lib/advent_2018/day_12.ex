83a3c08f1587353ce8e8c36d1f7ddcd19e978700
Duff OMelia <duff@omelia.org>
2018-12-19 17:28:42 -0500

    Implement 12B
    And refactor A to be simpler with a collecting parameter.

--- lib/advent_2018/day_12.ex
+++ lib/advent_2018/day_12.ex
@@ -1,32 +1,94 @@
-defmodule Advent2018.Day12 do
-  def part_a(pots, generation_count, input) do
-    {pots, padding_added_at_front} = generate(pots, generation_count, input)
+defmodule Advent2018.Day12.PotData do
+  defstruct pots: [],
+            notes: nil,
+            generations_processed: 0,
+            remaining_generations: nil,
+            previous_sum: 0,
+            previous_delta: 0,
+            padding_added_at_front: 0,
+            total_sum: 0
+end
 
-    Enum.zip(-padding_added_at_front..length(pots), pots)
+defmodule Advent2018.Day12 do
+  alias Advent2018.Day12.PotData
+
+  def part_a(pots, generation_count, input_notes) do
+    initialize(pots, input_notes, generation_count)
+    |> generate()
+    |> pot_sum()
+  end
+
+  def part_b(pots, input_notes) do
+    initialize(pots, input_notes, 50_000_000_000)
+    |> generate_until_same_delta()
+    |> pot_sum_using_same_delta()
+  end
+
+  def generate(%PotData{remaining_generations: 0} = data) do
+    data
+  end
+
+  def generate(data) do
+    data
+    |> next_generation()
+    |> generate()
+  end
+
+  def generate_until_same_delta(data) do
+    new_data = next_generation(data)
+    old_sum = pot_sum(data)
+    new_sum = pot_sum(new_data)
+    new_delta = new_sum - old_sum
+
+    if new_delta == data.previous_delta do
+      data
+    else
+      %PotData{new_data | previous_delta: new_delta, previous_sum: new_sum}
+      |> generate_until_same_delta()
+    end
+  end
+
+  defp decrement_remaining_generations(data) do
+    %PotData{data | remaining_generations: data.remaining_generations - 1}
+  end
+
+  defp increment_generations_processed(data) do
+    %PotData{data | generations_processed: data.generations_processed + 1}
+  end
+
+  defp increment_padding_count(data) do
+    %PotData{data | padding_added_at_front: data.padding_added_at_front + 3}
+  end
+
+  defp initialize(pots, input_notes, generation_count) do
+    %PotData{pots: String.codepoints(pots), notes: notes(input_notes), remaining_generations: generation_count}
+  end
+
+  defp next_generation(%PotData{pots: pots, notes: notes} = data) do
+    new_pots =
+      pots
+      |> padded()
+      |> Enum.chunk_every(5, 1)
+      |> Enum.map(fn each ->
+        Map.get(notes, each, ".")
+      end)
+
+    %PotData{data | pots: new_pots}
+    |> decrement_remaining_generations()
+    |> increment_generations_processed()
+    |> increment_padding_count()
+  end
+
+  defp pot_sum(%PotData{pots: pots, padding_added_at_front: padding}) do
+    -padding..length(pots)
+    |> Enum.zip(pots)
     |> Enum.filter(fn {_index, value} -> value == "#" end)
     |> Enum.map(&elem(&1, 0))
     |> Enum.sum()
   end
 
-  def generate(pots, generation_count, input) do
-    generate(String.codepoints(pots), generation_count, 0, notes(input))
-  end
-
-  def generate(pots, 0, padding_added_at_front, _) do
-    {pots, padding_added_at_front}
-  end
-
-  def generate(pots, generation_count, padding_added_at_front, notes) do
-    generate(next_gen(pots, notes), generation_count - 1, padding_added_at_front + 3, notes)
-  end
-
-  defp next_gen(pots, notes) do
-    pots
-    |> padded()
-    |> Enum.chunk_every(5, 1)
-    |> Enum.map(fn each ->
-      Map.get(notes, each, ".")
-    end)
+  defp pot_sum_using_same_delta(data) do
+    data.remaining_generations * data.previous_delta + data.previous_sum
   end
 
   defp padded(pots) do
