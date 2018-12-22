811101db419430d57fa39f50415a0a7282b92eb1
Duff OMelia <duff@omelia.org>
2018-12-22 10:17:24 -0500

    Implement 14B

--- lib/advent_2018/day_14.ex
+++ lib/advent_2018/day_14.ex
@@ -1,25 +1,49 @@
 defmodule Advent2018.Day14.Board do
-  defstruct scores: %{}, elf_1: 1, elf_2: 2, threshold: nil, needed_scores: nil, total_added: 0
+  defstruct scores: %{},
+            elf_1: 1,
+            elf_2: 2,
+            threshold: nil,
+            needed_scores: nil,
+            total_added: 0,
+            expected_pattern: nil,
+            last_five: []
 end
 
 defmodule Advent2018.Day14 do
   alias Advent2018.Day14.Board
 
   def part_a(scores, threshold) do
-    initialize(scores, threshold)
-    |> make_hot_chocolate
+    initialize_a(scores, threshold)
+    |> make_hot_chocolate_for_count
     |> final_ten_recipes
   end
 
-  defp make_hot_chocolate(%Board{needed_scores: needed, total_added: total_added} = board) when total_added >= needed do
+  def part_b(scores, expected_pattern) do
+    initialize_b(scores, expected_pattern)
+    |> make_hot_chocolate_for_pattern
+    |> recipe_count_needed
+  end
+
+  defp make_hot_chocolate_for_count(%Board{needed_scores: needed, total_added: total_added} = board) when total_added >= needed do
     board
   end
 
-  defp make_hot_chocolate(board) do
+  defp make_hot_chocolate_for_count(board) do
     board
     |> add_recipes
     |> move_elves
-    |> make_hot_chocolate
+    |> make_hot_chocolate_for_count
+  end
+
+  defp make_hot_chocolate_for_pattern(%Board{expected_pattern: same, last_five: same} = board) do
+    board
+  end
+
+  defp make_hot_chocolate_for_pattern(board) do
+    board
+    |> add_recipes
+    |> move_elves
+    |> make_hot_chocolate_for_pattern
   end
 
   defp move_elves(%Board{elf_1: elf_1, elf_2: elf_2} = board) do
@@ -41,18 +65,24 @@ defmodule Advent2018.Day14 do
     add_recipes(board, sum)
   end
 
-  defp add_recipes(%Board{scores: scores, total_added: total_added} = board, sum) when div(sum, 10) == 0 do
-    new_scores = Map.put(scores, total_added + 1, Integer.mod(sum, 10))
-    %Board{board | scores: new_scores, total_added: total_added + 1}
+  defp add_recipes(%Board{scores: scores, total_added: total_added, last_five: last_five} = board, sum) when div(sum, 10) == 0 do
+    value = Integer.mod(sum, 10)
+    new_last_five = (last_five ++ [value]) |> Enum.take(-5)
+    new_scores = Map.put(scores, total_added + 1, value)
+    %Board{board | scores: new_scores, total_added: total_added + 1, last_five: new_last_five}
   end
 
-  defp add_recipes(%Board{scores: scores, total_added: total_added} = board, sum) do
+  defp add_recipes(%Board{scores: scores, total_added: total_added, last_five: last_five} = board, sum) do
+    value_1 = div(sum, 10)
+    value_2 = Integer.mod(sum, 10)
+    new_last_five = (last_five ++ [value_1, value_2]) |> Enum.take(-5)
+
     new_scores =
       scores
       |> Map.put(total_added + 1, div(sum, 10))
       |> Map.put(total_added + 2, Integer.mod(sum, 10))
 
-    %Board{board | scores: new_scores, total_added: total_added + 2}
+    %Board{board | scores: new_scores, total_added: total_added + 2, last_five: new_last_five}
   end
 
   defp final_ten_recipes(%Board{scores: scores, threshold: threshold}) do
@@ -61,6 +91,10 @@ defmodule Advent2018.Day14 do
       Map.get(scores, each)
     end)
     |> Enum.join("")
+  end
+
+  defp recipe_count_needed(%Board{total_added: total_added}) do
+    total_added - 5
   end
 
   defp recipe_at(board, index) do
@@ -75,7 +109,12 @@ defmodule Advent2018.Day14 do
     recipe_at(board, board.elf_2)
   end
 
-  defp initialize([uno, dos], threshold) do
+  defp initialize_a([uno, dos], threshold) do
     %Board{scores: %{1 => uno, 2 => dos}, total_added: 2, threshold: threshold, needed_scores: threshold + 10}
+  end
+
+  defp initialize_b([uno, dos], expected_pattern) do
+    expected = String.split(expected_pattern, "", trim: true) |> Enum.map(&String.to_integer/1)
+    %Board{scores: %{1 => uno, 2 => dos}, total_added: 2, expected_pattern: expected, last_five: [uno, dos]}
   end
 end
--- test/lib/advent_2018/day_14_test.exs
+++ test/lib/advent_2018/day_14_test.exs
@@ -10,8 +10,20 @@ defmodule Advent2018.Day14Test do
     assert Day14.part_a([3, 7], 2018) == "5941429882"
   end
 
+  test "part_b" do
+    assert Day14.part_b([3, 7], "51589") == 9
+    assert Day14.part_b([3, 7], "01245") == 5
+    assert Day14.part_b([3, 7], "92510") == 18
+    assert Day14.part_b([3, 7], "59414") == 2018
+  end
+
   @tag :real
   test "part_a real" do
     assert Day14.part_a([3, 7], 554_401) == "3610281143"
+  end
+
+  @tag :real
+  test "part_b real" do
+    assert Day14.part_b([3, 7], "36102") == 1112
   end
 end
