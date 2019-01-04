a4f6335ac21c27a5624dd8576fa38d87eb473f1e
Duff OMelia <duff@omelia.org>
2019-01-04 16:40:15 -0500

    Day 18A

--- lib/advent_2018/day_18.ex
+++ lib/advent_2018/day_18.ex
@@ -1,5 +1,112 @@
 defmodule Advent2018.Day18 do
-  def part_a(_input) do
-    1147
+  def part_a(input) do
+    input
+    |> parse
+    |> transform(10)
+    |> resource_value
+  end
+
+  defp resource_value(map) do
+    count(map, :trees) * count(map, :lumberyard)
+  end
+
+  defp transform(map, times) do
+    Enum.reduce(1..times, map, fn _each, acc ->
+      transform(acc)
+    end)
+  end
+
+  defp transform(map) do
+    Enum.reduce(map, %{}, fn {coords, acre}, acc ->
+      Map.put(acc, coords, new_value(acre, coords, map))
+    end)
+  end
+
+  defp new_value(:open, coords, map) do
+    if adjacent_count(map, coords, :trees) >= 3 do
+      :trees
+    else
+      :open
+    end
+  end
+
+  defp new_value(:trees, coords, map) do
+    if adjacent_count(map, coords, :lumberyard) >= 3 do
+      :lumberyard
+    else
+      :trees
+    end
+  end
+
+  defp new_value(:lumberyard, coords, map) do
+    if adjacent_count(map, coords, :lumberyard) >= 1 && adjacent_count(map, coords, :trees) >= 1 do
+      :lumberyard
+    else
+      :open
+    end
+  end
+
+  defp adjacent_count(map, coords, target) do
+    Enum.count(adjacent_acres(map, coords), fn each ->
+      each == target
+    end)
+  end
+
+  defp adjacent_acres(map, {x, y}) do
+    [
+      Map.get(map, {x + 1, y}),
+      Map.get(map, {x - 1, y}),
+      Map.get(map, {x, y + 1}),
+      Map.get(map, {x, y - 1}),
+      Map.get(map, {x + 1, y + 1}),
+      Map.get(map, {x - 1, y - 1}),
+      Map.get(map, {x - 1, y + 1}),
+      Map.get(map, {x + 1, y - 1})
+    ]
+  end
+
+  defp count(map, target) do
+    Enum.count(map, fn {_, acre} -> acre == target end)
+  end
+
+  defp parse(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.with_index(1)
+    |> Enum.reduce(%{}, &parse_line/2)
+  end
+
+  defp parse_line({line, y}, acc) do
+    String.graphemes(line)
+    |> Enum.with_index(1)
+    |> Enum.reduce(acc, fn {char, x}, acc ->
+      Map.put(acc, {x, y}, atom_for_char(char))
+    end)
+  end
+
+  defp atom_for_char("."), do: :open
+  defp atom_for_char("#"), do: :lumberyard
+  defp atom_for_char("|"), do: :trees
+
+  defp char_for_atom(:open), do: "."
+  defp char_for_atom(:lumberyard), do: "#"
+  defp char_for_atom(:trees), do: "|"
+
+  def print(map) do
+    {{_, max}, _} = Enum.max_by(map, fn {{_x, y}, _atom} -> y end)
+
+    IO.write("\n")
+
+    Enum.each(1..max, fn y ->
+      Enum.each(1..max, fn x ->
+        IO.write(Map.get(map, {x, y}) |> char_for_atom)
+      end)
+
+      IO.write("\n")
+    end)
+
+    IO.write("\n")
+
+    map
   end
 end
--- test/lib/advent_2018/day_18_test.exs
+++ test/lib/advent_2018/day_18_test.exs
@@ -19,4 +19,10 @@ defmodule Advent2018.Day18Test do
 
     assert Day18.part_a(input) == 1147
   end
+
+  @tag :real
+  test "part_a real" do
+    input = File.read!("test/lib/advent_2018/input/day_18.txt")
+    assert Day18.part_a(input) == 598_416
+  end
 end
