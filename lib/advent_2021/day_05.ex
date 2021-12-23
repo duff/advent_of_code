d49e541fddb9f5dd7849c439f8c5daae74477c79
Duff OMelia <duff@omelia.org>
2021-12-23 12:08:17 -0500

    Add Day 05 part 2

--- lib/advent_2021/day_05.ex
+++ lib/advent_2021/day_05.ex
@@ -12,9 +12,23 @@ defmodule Advent2021.Day05.Segment do
     %Segment{x1: x1, y1: y1, x2: x2, y2: y2}
   end
 
-  def points(%Segment{x1: same_x, y1: y1, x2: same_x, y2: y2}), do: for(y <- y1..y2, do: {same_x, y})
-  def points(%Segment{x1: x1, y1: same_y, x2: x2, y2: same_y}), do: for(x <- x1..x2, do: {x, same_y})
-  def points(_), do: []
+  def hv_points(%Segment{x1: same_x, y1: y1, x2: same_x, y2: y2}), do: for(y <- y1..y2, do: {same_x, y})
+  def hv_points(%Segment{x1: x1, y1: same_y, x2: x2, y2: same_y}), do: for(x <- x1..x2, do: {x, same_y})
+  def hv_points(_), do: []
+
+  def diagonal_points(%Segment{x1: same, y1: _, x2: same, y2: _}), do: []
+
+  def diagonal_points(%Segment{x1: x1, y1: y1, x2: x2, y2: y2}) when abs((y2 - y1) / (x2 - x1)) == 1 do
+    for x <- x1..x2,
+        y <- y1..y2,
+        x - x1 != 0,
+        abs((y - y1) / (x - x1)) == 1 do
+      {x, y}
+    end
+    |> Enum.concat([{x1, y1}])
+  end
+
+  def diagonal_points(_), do: []
 
   defp split_map(string) do
     string
@@ -28,12 +42,31 @@ defmodule Advent2021.Day05 do
 
   def overlapping_point_count(input) do
     input
-    |> String.split("\n", trim: true)
-    |> Enum.map(&Segment.new/1)
-    |> Enum.map(&Segment.points/1)
+    |> segments()
+    |> Enum.map(&Segment.hv_points/1)
     |> List.flatten()
     |> Enum.frequencies()
     |> Enum.filter(fn {_, count} -> count > 1 end)
     |> Enum.count()
+  end
+
+  def overlapping_point_count_with_diagonals(input) do
+    input
+    |> segments()
+    |> all_points
+    |> List.flatten()
+    |> Enum.frequencies()
+    |> Enum.filter(fn {_, count} -> count > 1 end)
+    |> Enum.count()
+  end
+
+  defp all_points(segments) do
+    Enum.map(segments, &Segment.hv_points/1) ++ Enum.map(segments, &Segment.diagonal_points/1)
+  end
+
+  defp segments(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&Segment.new/1)
   end
 end
--- test/lib/advent_2021/day_05_test.exs
+++ test/lib/advent_2021/day_05_test.exs
@@ -13,9 +13,28 @@ defmodule Advent2021.Day05Test do
     assert Day05.overlapping_point_count(input) == 6572
   end
 
-  test "points" do
-    assert Segment.points(%Segment{x1: 7, y1: 0, x2: 7, y2: 4}) == [{7,0}, {7,1}, {7,2}, {7,3}, {7,4}]
-    assert Segment.points(%Segment{x1: 3, y1: 4, x2: 1, y2: 4}) == [{3,4}, {2,4}, {1,4}]
+  test "overlapping_point_count_with_diagonals" do
+    assert Day05.overlapping_point_count_with_diagonals(input()) == 12
+  end
+
+  @tag :real_data_slow
+  test "overlapping_point_count_with_diagonals real" do
+    input = File.read!("test/lib/advent_2021/input/day_05.txt")
+    assert Day05.overlapping_point_count_with_diagonals(input) == 21466
+  end
+
+  describe "Segment" do
+    test "hv_points" do
+      assert Segment.hv_points(%Segment{x1: 7, y1: 0, x2: 7, y2: 4}) == [{7, 0}, {7, 1}, {7, 2}, {7, 3}, {7, 4}]
+      assert Segment.hv_points(%Segment{x1: 3, y1: 4, x2: 1, y2: 4}) == [{3, 4}, {2, 4}, {1, 4}]
+      assert Segment.hv_points(%Segment{x1: 3, y1: 7, x2: 1, y2: 4}) == []
+    end
+
+    test "diagonal_points" do
+      assert Segment.diagonal_points(%Segment{x1: 1, y1: 1, x2: 3, y2: 3}) == [{2, 2}, {3, 3}, {1, 1}]
+      assert Segment.diagonal_points(%Segment{x1: 9, y1: 7, x2: 7, y2: 9}) == [{8, 8}, {7, 9}, {9, 7}]
+      assert Segment.diagonal_points(%Segment{x1: 3, y1: 7, x2: 1, y2: 4}) == []
+    end
   end
 
   defp input do
