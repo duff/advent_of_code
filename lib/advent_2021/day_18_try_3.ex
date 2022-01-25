e27645386be3d36e78781463076cf4f7d6fbad85
Duff OMelia <duff@omelia.org>
2022-01-25 16:00:14 -0500

    Try a tuple approach
    Maybe I'll be able to go back to string for explode.

--- lib/advent_2021/day_18_try_3.ex
+++ lib/advent_2021/day_18_try_3.ex
@@ -1,14 +1,16 @@
 defmodule Advent2021.Day18.Try3 do
   def parse(input) do
     input
+    |> String.replace("[", "{")
+    |> String.replace("]", "}")
     |> Code.string_to_quoted!()
   end
 
   def add(one, two) do
-    [one ++ two]
+    {one,two}
   end
 
-  def magnitude([left, right]) do
+  def magnitude({left, right}) do
     magnitude(left) * 3 + magnitude(right) * 2
   end
 
@@ -16,24 +18,24 @@ defmodule Advent2021.Day18.Try3 do
     value
   end
 
-  def split([left, right]) do
+  def split({left, right}) do
     case split(left) do
       {false, left} ->
         case split(right) do
           {false, right} ->
-            {false, [left, right]}
+            {false, {left, right}}
 
           {true, right} ->
-            {true, [left, right]}
+            {true, {left, right}}
         end
 
       {true, left} ->
-        {true, [left, right]}
+        {true, {left, right}}
     end
   end
 
   def split(val) when is_integer(val) and val > 9 do
-    {true, [div(val, 2), ceil(val / 2)]}
+    {true, {div(val, 2), ceil(val / 2)}}
   end
 
   def split(val), do: {false, val}
