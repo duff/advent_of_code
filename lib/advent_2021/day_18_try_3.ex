30c77e952cfda5fb6a7ca483dd5b59292d7a1af3
Duff OMelia <duff@omelia.org>
2022-01-26 11:35:03 -0500

    Add explode to Day 18

--- lib/advent_2021/day_18_try_3.ex
+++ lib/advent_2021/day_18_try_3.ex
@@ -7,7 +7,7 @@ defmodule Advent2021.Day18.Try3 do
   end
 
   def add(one, two) do
-    {one,two}
+    {one, two}
   end
 
   def magnitude({left, right}) do
@@ -39,4 +39,78 @@ defmodule Advent2021.Day18.Try3 do
   end
 
   def split(val), do: {false, val}
+
+  def explode(ast) do
+    list = as_list(ast)
+    index_left = find_explosive(list)
+
+    if index_left do
+      {true, actually_explode(list, index_left)}
+    else
+      {false, ast}
+    end
+  end
+
+  defp actually_explode(list, index_left) do
+    index_right = index_left + 2
+
+    list
+    |> splode_right(index_right)
+    |> splode_left(index_left)
+    |> replace_exploding_pair_with_zero(index_left, index_right)
+    |> Enum.join()
+    |> Code.string_to_quoted!()
+  end
+
+  defp splode_right(list, index_right) do
+    index_next_number =
+      list
+      |> Enum.with_index()
+      |> Enum.find_index(fn {element, index} -> index > index_right and is_integer(element) end)
+
+    if index_next_number do
+      List.update_at(list, index_next_number, fn existing -> existing + Enum.at(list, index_right) end)
+    else
+      list
+    end
+  end
+
+  defp splode_left(list, index_left) do
+    found =
+      list
+      |> Enum.with_index()
+      |> Enum.slice(0..(index_left - 1))
+      |> Enum.reverse()
+      |> Enum.find(fn {element, _index} -> is_integer(element) end)
+
+    case found do
+      {_element, index} -> List.update_at(list, index, fn existing -> existing + Enum.at(list, index_left) end)
+      nil -> list
+    end
+  end
+
+  defp find_explosive(list), do: find_explosive(list, 0, 0)
+  defp find_explosive(["{" | rest], index, depth), do: find_explosive(rest, index + 1, depth + 1)
+  defp find_explosive(["}" | rest], index, depth), do: find_explosive(rest, index + 1, depth - 1)
+  defp find_explosive([left, ",", right, "}" | _], index, 5) when is_integer(left) and is_integer(right), do: index
+  defp find_explosive([_ | rest], index, depth), do: find_explosive(rest, index + 1, depth)
+  defp find_explosive([], _index, _depth), do: false
+
+  defp replace_exploding_pair_with_zero(list, index_left, index_right) do
+    Enum.slice(list, 0..(index_left - 2)) ++ Enum.slice(list, (index_right + 2)..length(list))
+    |> List.insert_at(index_left - 1, 0)
+  end
+
+  defp as_list(ast) do
+    ast
+    |> Macro.to_string()
+    |> String.replace(" ", "")
+    |> String.graphemes()
+    |> Enum.map(fn e ->
+      case Integer.parse(e) do
+        :error -> e
+        {number, _rem} -> number
+      end
+    end)
+  end
 end
--- test/lib/advent_2021/day_18_try_3_test.exs
+++ test/lib/advent_2021/day_18_try_3_test.exs
@@ -8,19 +8,19 @@ defmodule Advent2021.Day18.Try3Test do
   end
 
   test "parse with left pair" do
-    assert Try3.parse("[[1,2],3]") == {{1,2},3}
+    assert Try3.parse("[[1,2],3]") == {{1, 2}, 3}
   end
 
   test "parse with right pair" do
-    assert Try3.parse("[9,[8,7]]") == {9,{8,7}}
+    assert Try3.parse("[9,[8,7]]") == {9, {8, 7}}
   end
 
   test "parse with both pairs" do
-    assert Try3.parse("[[1,9],[8,5]]") == {{1,9},{8,5}}
+    assert Try3.parse("[[1,9],[8,5]]") == {{1, 9}, {8, 5}}
   end
 
   test "parse bigger" do
-    assert Try3.parse("[[[[1,2],[3,4]],[[5,6],[7,8]]],9]") == {{{{1,2},{3,4}},{{5,6},{7,8}}},9}
+    assert Try3.parse("[[[[1,2],[3,4]],[[5,6],[7,8]]],9]") == {{{{1, 2}, {3, 4}}, {{5, 6}, {7, 8}}}, 9}
   end
 
   test "add" do
@@ -38,16 +38,24 @@ defmodule Advent2021.Day18.Try3Test do
     assert Try3.magnitude({{{{8, 7}, {7, 7}}, {{8, 6}, {7, 7}}}, {{{0, 7}, {6, 6}}, {8, 7}}}) == 3488
   end
 
-  test "split" do
-    assert Try3.split({{{{{4, 3}, 4}, 4}, {7, {{8, 4}, 9}}}, {1, 1}}) == {false, {{{{{4, 3}, 4}, 4}, {7, {{8, 4}, 9}}}, {1, 1}}}
+  test "split succeeds" do
     assert Try3.split({{{{0, 7}, 4}, {15, {0, 13}}}, {1, 1}}) == {true, {{{{0, 7}, 4}, {{7, 8}, {0, 13}}}, {1, 1}}}
     assert Try3.split({{{{0, 7}, 4}, {{7, 8}, {0, 13}}}, {1, 1}}) == {true, {{{{0, 7}, 4}, {{7, 8}, {0, {6, 7}}}}, {1, 1}}}
   end
 
-  # test "explode" do
-  #   assert Try3.explode(Try3.new({{{{{9,8},1},2},3},4})) == Try3.new({{{{0,9},2},3},4})
-  #   # assert Try3.explode(Try3.new({{6,{5,{4,{3,2}}}},1})) == Try3.new({{6,{5,{7,0}}},3})
-  #   # assert Try3.explode(Try3.new({{3,{2,{1,{7,3}}}},{6,{5,{4,{3,2}}}}})) == Try3.new({{3,{2,{8,0}}},{9,{5,{4,{3,2}}}}})
-  #   # assert Try3.explode(Try3.new({{3,{2,{8,0}}},{9,{5,{4,{3,2}}}}})) == Try3.new({{3,{2,{8,0}}},{9,{5,{7,0}}}})
-  # end
+  test "split not needed" do
+    assert Try3.split({{{{{4, 3}, 4}, 4}, {7, {{8, 4}, 9}}}, {1, 1}}) == {false, {{{{{4, 3}, 4}, 4}, {7, {{8, 4}, 9}}}, {1, 1}}}
+  end
+
+  test "explode succeeds" do
+    assert Try3.explode({{{{{9, 8}, 1}, 2}, 3}, 4}) == {true, {{{{0, 9}, 2}, 3}, 4}}
+    assert Try3.explode({{6, {5, {4, {3, 2}}}}, 1}) == {true, {{6, {5, {7, 0}}}, 3}}
+    assert Try3.explode({7, {6, {5, {4, {3, 2}}}}}) == {true, {7, {6, {5, {7, 0}}}}}
+    assert Try3.explode({{3, {2, {1, {7, 3}}}}, {6, {5, {4, {3, 2}}}}}) == {true, {{3, {2, {8, 0}}}, {9, {5, {4, {3, 2}}}}}}
+    assert Try3.explode({{3, {2, {8, 0}}}, {9, {5, {4, {3, 2}}}}}) == {true, {{3, {2, {8, 0}}}, {9, {5, {7, 0}}}}}
+  end
+
+  test "explode not needed" do
+    assert Try3.explode({{{{0,7},4},{{7,8},{6,0}}},{8,1}}) == {false, {{{{0,7},4},{{7,8},{6,0}}},{8,1}}}
+  end
 end
